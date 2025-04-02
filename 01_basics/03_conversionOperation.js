51f8dc7042f0f363857fd7128cb146a2ca14d481
@Megha <138016687+shakyamegha246@users.noreply.github.com>
2025-04-02 17:29:16 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,4 +31,53 @@ console.log(typeof stringNumber);
 
 /*A number can also be converted into string
 */
-  
+ 
+
+/* ************Operations************ */
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+ 2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);
+console.log(2%3);
+console.log(2/3);
+
+let str1 = "hello";
+let str2 = " Megha";
+let str3 = str1 + str2;
+console.log(str3); //Addition of two strings
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (2+4) *5 %3); // Not a good way to write code, use proper braceses
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1 ,  num2, num3 ;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+// link to study  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+ 
