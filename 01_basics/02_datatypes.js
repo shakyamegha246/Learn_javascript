1eae8b012d7ee539e55b216233632dc97c3bf585
@Megha <138016687+shakyamegha246@users.noreply.github.com>
2025-04-02 16:38:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treates all JS code as newer version
+
+//alert("Hello Megha");   // we are using Node js not browser , Thats doesn't work here.
+console.log("Hello Megha");
+
+console.log( 3
+    + 3
+) // code readability should be high
+
+let name = "Megha";
+let age = 19;
+let isLoggedIn = false;
+let state = null;
+
+/*There are following types of data types available in javascript
+number => 2 to the power 53
+bigint
+string => written in ""
+boolean => true / false
+null => standalone value/ representation of an empty value
+undefined => when value is not defined
+symbol => unique values
+
+
+object => user defined data type
+*/
+
+console.log(typeof(age));
+console.log(typeof( null));  // object
+console.log(typeof(undefined));  // undefined
