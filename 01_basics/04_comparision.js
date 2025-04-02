63ea7a43c1330c2e923539590cb28ad7495328c6
@Megha <138016687+shakyamegha246@users.noreply.github.com>
2025-04-02 17:40:44 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,42 @@
+// console.log(2 > 1);  true
+// console.log(2 >=1);  true
+// console.log(2 <1);  false
+// console.log(1 == 2);  false
+// console.log(2 != 1); true
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+//While comparing two values both the values must be in same data type , otherwise the answer is not predictable
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log( null >= 0);
+
+//Avoid these types of comparisions while writing the code because it will give different results.
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//Strict check => ===  It strictly checks the data type of both the values.
+console.log("2" ===2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
