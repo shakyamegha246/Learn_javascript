86788ac31e9d87845643698b96e25c563b939686
@Megha <138016687+shakyamegha246@users.noreply.github.com>
2025-04-03 18:19:41 +0000

    datatypesSummary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+//Data is categorised on the basics of how it is stored in memory. It is of two type - Primitive and Non- Primitive.
+/* 
+Primitive Data Types- It is of seven types -> String, Number, Boolean, null, undefined,Symbol and BigInt.
+
+Reference Type/ Non-Primitive -> Arrays, Objects, Functions.
+
+Javascript is a dynamically typed language.
+*/
+
+const score = 100;
+const scoreValue = 100.8;
+
+const isLoggedIn = false;
+const outsidetemp = null;
+let userEmail;
+
+const id = Symbol('1234')
