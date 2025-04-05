9b5c3ec4d8c4e9745c3f8b77fbc19661c4641573
@Megha <138016687+shakyamegha246@users.noreply.github.com>
2025-04-05 15:02:08 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "megha";
+const repoCount = 50;
+
+console.log( name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('meghashakya');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
